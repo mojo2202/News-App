92c6609b4538ccc18b5dcc631c4489eb28108b44
Manoj <manoj.gahlawat@gmail.com>
2016-04-26 13:06:20 +0200

    First push
    first push of complete file system

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+var testVariable; 
+var isUndefined = (testVariable === Undefined);
+var isNull = (testVariable === null);
+var isEmpty = (testVariable === '');
+
+document.getElementById('isUndefined').innerHTML = isUndefined;
+document.getElementById('isNull').innerHTML = isNull;
+document.getElementById('isEmpty').innerHTML = isEmpty;
+
+var testVariable = 'ABC';
+var testVariable = 'null';
+
